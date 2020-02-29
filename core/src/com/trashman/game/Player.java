1ce71c704030d9bb0e034c61e4dcc2d58bef0a0c
Yuqing Wang <46657555+YUQINGWANG-0@users.noreply.github.com>
2020-02-29 23:04:42 +0000

    add player, bin, trash class

---
+++ core/src/com/trashman/game/Player.java
@@ -0,0 +1,61 @@
+package com.trashman.game;
+
+public class Player {
+    private Position position;
+    private Trash trash;
+    // private boolean bag;
+
+    public Player(Position position){
+        this.position = position;
+        trash = null;
+    }
+
+    public void pickup (Trash newtrash){
+        trash = newtrash;
+
+    }
+
+    public boolean checkbag(){
+        if (trash == null){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void putdown(){
+        trash = null;
+
+    }
+
+    public Position getposition(){
+        return position;
+    }
+
+    public void moveleft(){
+        // depend on keyboard input
+        position.setX(position.getX()-1);
+    }
+    public void moveright(){
+        // depend on keyboard input
+        position.setX(position.getX()+1);
+    }
+    public void moveup(){
+        // depend on keyboard input
+        position.setY(position.getY()+1);
+    }
+    public void movedown(){
+        // depend on keyboard input
+        position.setY(position.getY()-1);
+    }
+
+    public boolean fullbag(){
+        if (trash == null){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
